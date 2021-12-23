cb8dbab4b01e7c5a5cc6a6f7d4b0e62ad993e244
KimByungHun <k01087831451@gmail.com>
2021-12-24 03:50:50 +0900

    헤더 탭 메뉴

---
+++ js/header.js
@@ -0,0 +1,35 @@
+const gnb_li = document.querySelectorAll("#gnb>li");
+
+
+//1뎁스 li마다 반복을 돌면서 이벤트 연결
+gnb_li.forEach((li, index)=>{
+    //각 1뎁스 li에 마우스 호버시 서브패널 보이기
+    li.addEventListener("mouseenter", e=>{
+        e.currentTarget.querySelector(".sub").style.display="block";
+    })
+    //각 1뎁스 li에 마우스 호버시 서브패널 숨기기
+    li.addEventListener("mouseleave",e=>{
+        e.currentTarget.querySelector(".sub").style.display="none";
+    })
+    //각 1뎁스 li에 포커스 발생시 해당요소의 자식 서브패널 보이기
+    li.addEventListener("focusin", e=>{
+        e.currentTarget.querySelector(".sub").style.display="block";
+    })
+    //각 1depthli의 .sub ul까지 탐색후 마지막 자식 li 요소를 선택해서
+    //해당 요소에서의 포커스가 떠나면 제일 가까운 부모여소인 sub를 숨김처리
+    li.querySelector(".sub ul").lastElementChild.addEventListener("focusout", e=>{
+        e.currentTarget.closest(".sub").style.display="none"
+    })
+})
+
+
+//스킵네비게이션 이벤트 
+const btns = document.querySelectorAll("#skip a");
+btns.forEach((btn, index)=>{
+    btn.addEventListener("focusin",e=>{
+        e.currentTarget.classList.add("on");
+    })
+    btn.addEventListener("focusout",e=>{
+        e.currentTarget.classList.remove("on");
+    })
+})
