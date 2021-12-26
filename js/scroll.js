4956b708b7e696ccd5ff1c72c73ff1fe1b356cfa
KimByungHun <k01087831451@gmail.com>
2021-12-26 22:33:11 +0900

    스크롤 활성화

--- js/scroll.js
+++ js/scroll.js
@@ -5,7 +5,8 @@ let posArr =[];
  for(let el of sections){
      posArr.push(el.offsetTop);
  }
- console.log(posArr);
+
+// 스크롤 버튼 클릭
 
  lis.forEach((el,index)=>{
      el.addEventListener("click", e=>{
@@ -22,3 +23,18 @@ let posArr =[];
         e.currentTarget.classList.add("on");
      })
  })
+ 
+
+ // 브라우저 스크롤시
+window.addEventListener("scroll", e=>{
+    let scroll = window.scrollY || window.pageYOffset;
+
+    sections.forEach((el, index)=>{
+        if(scroll >= posArr[index]){
+            for(let el of lis){
+                el.classList.remove("on");
+            }
+            lis[index].classList.add("on");
+        }
+    })
+});
